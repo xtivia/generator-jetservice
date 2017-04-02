8e25a7c8435f96cb131999c32cde23e94f323762
Michael Freeman <mfreeman@xtivia.com>
2017-04-02 09:53:29 -0500

    More cleanup

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,9 +3,6 @@ var yosay = require('yosay');
 var fs = require('fs-extra');
 var path = require('path');
 
-var cjs = require('./cjs');
-var jaxrs = require('./jaxrs');
-
 module.exports = class extends Generator {
   
   initializing() {
@@ -52,7 +49,14 @@ module.exports = class extends Generator {
                 return "Path must begin with a slash, e.g. /org.jetservice.abcd"
               } else return true;
           }
-			  }
+			  },
+        {
+          required: true,
+          name: 'sgdxp',
+          type: 'confirm',
+          default: false,
+          message: 'Include Service Guard?'
+        }
 		];
 
     return this.prompt(prompts)
@@ -60,14 +64,35 @@ module.exports = class extends Generator {
 				self.props = answers;
 				self.config.set(self.props);
 			});
-    
   }
 
   writing() {
-    this.props.framework = 'JAXRS';
+    
     this.props.serviceFileName = 'index.js';
     this.config.set(this.props);
-    jaxrs.generate(this);
+    
+    this.fs.copyTpl(this.templatePath('jaxrs/settings.gradle'),
+        this.destinationPath('settings.gradle'), this.props);
+    this.fs.copy(this.templatePath('jaxrs/gradle.properties'),
+        this.destinationPath('gradle.properties'));
+    this.fs.copy(this.templatePath('jaxrs/deploy.jar'),
+        this.destinationPath('buildlibs/deploy.jar'));
+
+    if (this.props.sgdxp) {
+      this.fs.copy(this.templatePath('jaxrs/sgdxp/sgdxp.jar'),
+          this.destinationPath('sgdxp/sgdxp.jar'));
+      this.fs.copy(this.templatePath('jaxrs/sgdxp/sgdxp.README'),
+          this.destinationPath('sgdxp/README'));
+      this.fs.copy(this.templatePath('jaxrs/sgdxp/build.gradle'),
+          this.destinationPath('build.gradle'));
+      this.fs.copyTpl(this.templatePath('jaxrs/sgdxp/*.java'),
+          this.destinationPath('src/main/java/org/jetservice'), this.props);
+    } else {
+      this.fs.copy(this.templatePath('jaxrs/build.gradle'),
+          this.destinationPath('build.gradle'));
+      this.fs.copyTpl(this.templatePath('jaxrs/src/*.java'),
+          this.destinationPath('src/main/java/org/jetservice'), this.props);
+    }
   }
   
 }
